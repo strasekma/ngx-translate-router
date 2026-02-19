41ed5ef3953a3ca06518fc7104e485bdec3313d0
Bojan <bojan.kogoj@geneplanet.com>
2026-02-19 09:33:18 +0100

    Added prettier and reformatted some of the files

--- src/app/test4/yoyo/yoyo.component.ts
+++ src/app/test4/yoyo/yoyo.component.ts
@@ -3,20 +3,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { LocalizeRouterPipe } from '@gilsdav/ngx-translate-router';
 
 @Component({
-    selector: 'app-yoyo',
-    templateUrl: './yoyo.component.html',
-    styleUrls: ['./yoyo.component.css'],
-    imports: [
-        RouterOutlet,
-        RouterLink,
-        LocalizeRouterPipe
-    ]
+  selector: 'app-yoyo',
+  templateUrl: './yoyo.component.html',
+  styleUrls: ['./yoyo.component.css'],
+  imports: [RouterOutlet, RouterLink, LocalizeRouterPipe],
 })
 export class YoyoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
